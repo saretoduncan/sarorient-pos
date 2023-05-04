3e9973e8fc5d3ec5a15cd5758df1d1649e4279e2
= <=>
2023-05-04 20:04:19 +0300

    add validation to login form

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,16 +1,33 @@
+"use client";
 import React from "react";
 import logo from "../../public/pos_logo.png";
+import { SubmitHandler, useForm } from "react-hook-form";
+import { ILogin } from "../interfaces/frontendInterface/ILogin";
+
 const Login: React.FC<{}> = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<ILogin>();
+
+  const handleLoginSubmit: SubmitHandler<ILogin> = (loginInfo) => {
+    console.log(loginInfo);
+  };
   return (
     <>
       <section className='grid w-screen h-screen'>
         <section className='justify-self-center self-center border rounded p-3'>
-          <div className="grid">
-            <img src={logo.src} alt="pos logo image" className="justify-self-center w-[100px]" />
-            
-          </div>
+          <figure className='grid'>
+            <img
+              src={logo.src}
+              alt='pos logo image'
+              className='justify-self-center w-[100px]'
+            />
+            <figcaption className='text-center'>Sarorient POS</figcaption>
+          </figure>
           <div>
-            <form action='' method='POST'>
+            <form onSubmit={handleSubmit(handleLoginSubmit)}>
               <div className='flex flex-col mb-3'>
                 <label htmlFor='username'>Username</label>
                 <input
@@ -19,7 +36,15 @@ const Login: React.FC<{}> = () => {
                   type='text'
                   required
                   className=' border border-gray-200 rounded focus:outline-none focus:ring focus:border-blue-500 focus:rounded-sm '
+                  {...register("username", {
+                    required: true,
+                  })}
                 />
+                {errors.username && (
+                  <span className='text-red-500 text-sm'>
+                    {errors.username.message}
+                  </span>
+                )}
               </div>
               <div className='flex flex-col mb-3'>
                 <label>Password</label>
@@ -28,7 +53,15 @@ const Login: React.FC<{}> = () => {
                   placeholder='Password'
                   type='password'
                   required
+                  {...register("password", {
+                    required: true,
+                  })}
                 />
+                {errors.password && (
+                  <span className='text-red-500 text-sm'>
+                    {errors.password.message}
+                  </span>
+                )}
               </div>
               <div>
                 <button
