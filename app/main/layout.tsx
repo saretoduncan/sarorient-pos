b06e09dadc3d82a65fa0f2f9c904d1afbf047629
= <=>
2023-05-06 11:12:14 +0300

    remove border

--- app/main/layout.tsx
+++ app/main/layout.tsx
@@ -9,10 +9,10 @@ export default function MainLayout({
     <>
       <section className="grid grid-cols-6 h-screen">
         {" "}
-        <aside className="col-span-1 border border-orange-500 pt-3 bg-gray-800  text-white">
+        <aside className="col-span-1 pt-3 bg-gray-800  text-white">
           <SideBar />
         </aside>
-        <main className="col-span-5 border border-yellow-500">{children}</main>
+        <main className="col-span-5 ">{children}</main>
       </section>
     </>
   );
