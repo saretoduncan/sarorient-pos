c4dc76f140aa9e62b738e5f64c26cf7db6fb8fb8
Duncan <duncan.moiyo@gmail.com>
2023-05-10 17:22:31 +0300

    modify pos page

---
+++ app/components/Ordered_products.tsx
@@ -0,0 +1,19 @@
+export default function Ordered_products() {
+  return (
+    <>
+      <section className="grid grid-cols-8 bg-gray-300 mt-2 px-1 py-2 font-bold">
+      <div>No.</div>
+        <div>Product</div>
+        <div>Code</div>
+        <div>Brand</div>
+        <div>Qty</div>
+        <div>Unit</div>
+        <div>Price</div>
+        <div>Action</div>
+      </section>
+      <section>
+
+      </section>
+    </>
+  );
+}
--- app/main/pos/page.tsx
+++ app/main/pos/page.tsx
@@ -1,20 +1,21 @@
+import Ordered_products from "@/app/components/Ordered_products";
 import React from "react";
 
 const page = () => {
   return (
     <>
       <main className="grid grid-cols-6 h-screen pt-3">
-        <section className="col-span-4 border border-red-500 px-2">
+        <section className="col-span-4  px-2">
           <div className="bg-gray-800 ps-2 py-2">
             <h4 className="text-white font-bold">ORDERED PRODUCTS</h4>
           </div>
+          <Ordered_products />
         </section>
-        <section className="col-span-2 border border-yellow-500 px-1 ">
+        <section className="col-span-2  px-1 ">
           <div className="ps-2 py-3 bg-green-600">
-             <h5 className="text-white font-bold">Total: 0.00</h5>
+            <h5 className="text-white font-bold">Total: 0.00</h5>
           </div>
-         
-        </section> 
+        </section>
       </main>
     </>
   );
