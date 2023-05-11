d27362a86245f643e492004759afaece29ea6fef
Duncan <duncan.moiyo@gmail.com>
2023-05-11 15:55:35 +0300

    complete pos page design

--- app/main/pos/page.tsx
+++ app/main/pos/page.tsx
@@ -18,25 +18,27 @@ const page = () => {
         <section className="col-span-2  px-1 ">
           <div className="bg-white h-[100%]">
             <div className="ps-2 py-3 bg-green-600">
-              <h5 className="text-white font-bold">Total: 0.00</h5>
+              <h5 className="text-white font-bold text-[30px] ">Total: 0.00</h5>
             </div>
 
             <div className="px-2 bg-gray-200 mb-3">
               <h5>Payment Method</h5>
               <PaymentMethod />
             </div>
-            <div className={`px-2 `}>
-              {usePaymentMethodStore((state) => state.isMpesa)?(<div
-                className={`flex flex-col mb-2 `}
-              >
-                <label htmlFor="phoneNumber">Phone number</label>
-                <input
-                  type="number"
-                  id="phoneNumber"
-                  placeholder="+254 7xxxxxxx"
-                  className=" ring-1 ring-green-100 focus:border-none focus:outline-none focus:ring-2 rounded focus:ring-green-400 px-2"
-                />
-              </div>):""}
+            <div className="px-2">
+              {usePaymentMethodStore((state) => state.isMpesa) ? (
+                <div className={`flex flex-col mb-2`}>
+                  <label htmlFor="phoneNumber">Phone number</label>
+                  <input
+                    type="number"
+                    id="phoneNumber"
+                    placeholder="+254 7xxxxxxx"
+                    className=" ring-1 ring-green-100 focus:border-none focus:outline-none focus:ring-2 rounded focus:ring-green-400 px-2"
+                  />
+                </div>
+              ) : (
+                ""
+              )}
               <div className="flex flex-col mb-2">
                 <label htmlFor="payment">Payment</label>
                 <input
@@ -56,7 +58,7 @@ const page = () => {
             </div>
             <div className="px-2 mt-3">
               <button className="text-white bg-blue-500 w-[100%] rounded py-1 font-bold">
-                save
+                Make Payment
               </button>
             </div>
           </div>
