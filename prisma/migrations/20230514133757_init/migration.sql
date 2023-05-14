3926405ed16eff994263838c55d82609a018a164
Duncan <duncan.moiyo@gmail.com>
2023-05-14 16:49:55 +0300

    modify schema

---
+++ prisma/migrations/20230514133757_init/migration.sql
@@ -0,0 +1,63 @@
+-- CreateEnum
+CREATE TYPE "Role" AS ENUM ('EDITOR', 'ADMIN');
+
+-- CreateTable
+CREATE TABLE "User" (
+    "id" TEXT NOT NULL,
+    "username" TEXT NOT NULL,
+    "email" TEXT,
+    "phoneNumber" TEXT,
+    "password" TEXT NOT NULL,
+    "role" "Role" NOT NULL DEFAULT 'EDITOR',
+    "isActive" BOOLEAN NOT NULL DEFAULT true,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updateAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Product" (
+    "id" TEXT NOT NULL,
+    "productName" TEXT NOT NULL,
+    "code" TEXT NOT NULL,
+    "brand" TEXT NOT NULL,
+    "buyingPrice" DOUBLE PRECISION NOT NULL,
+    "sellingPrice" DOUBLE PRECISION NOT NULL,
+    "Qty" INTEGER NOT NULL,
+    "unit" TEXT NOT NULL DEFAULT 'PC',
+    "userId" TEXT,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updateAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Product_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "SoldProducts" (
+    "id" TEXT NOT NULL,
+    "boughtAt" DOUBLE PRECISION NOT NULL,
+    "soldAt" DOUBLE PRECISION NOT NULL,
+    "remainingQty" INTEGER NOT NULL,
+    "productId" TEXT,
+    "userId" TEXT,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updateAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "SoldProducts_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_username_key" ON "User"("username");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
+
+-- AddForeignKey
+ALTER TABLE "Product" ADD CONSTRAINT "Product_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "SoldProducts" ADD CONSTRAINT "SoldProducts_productId_fkey" FOREIGN KEY ("productId") REFERENCES "Product"("id") ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "SoldProducts" ADD CONSTRAINT "SoldProducts_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE SET NULL ON UPDATE CASCADE;
